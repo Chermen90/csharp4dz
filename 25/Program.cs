c8db01bc8f352a9d402ec519ebf08fae640a4325
st202222 <zarina.kochisova@gmail.com>
2022-09-30 23:18:57 -0400

    added files

---
+++ 25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+//просим пользователя ввести первое число
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse (Console.ReadLine ());
+
+// и второе число
+Console.WriteLine("Введите второе число: ");
+int secNumber = int.Parse (Console.ReadLine ());
+
+//выводим результат функции на экран
+Console.WriteLine ($"Число {firstNumber} в степени {secNumber} равно {GetResult(firstNumber, secNumber)}");
+
+
+//функция принимает два элемента int и выдаёт результат в виде double
+double GetResult (int firstNumber, int secNumber)
+{   double result = Math.Pow(firstNumber, secNumber);   
+    return result;
+}
---
+++ 29/Program.cs
@@ -0,0 +1,30 @@
+﻿//просим пользователя ввести длину массива и 
+//записываем её в переменную length
+Console.WriteLine("Введите длину массива: ");
+int length = int.Parse (Console.ReadLine ());
+
+//объявляем переменную array, куда положим результат функции
+int [] array = GetNum ();
+
+//объявляем функцию, кот-я возвращает int
+int [] GetNum ()
+{   
+    //объявляем переменные
+    // в number будем записывать числа от пользователя
+    // в arr - элементы массива
+    int number = 0;
+    int [ ] arr = new int [length];
+    //прописываем цикл for, который i < length раз получает числа от пользователя
+    //и добавляет их в массив
+    for(int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число: ");
+        number = int.Parse (Console.ReadLine ());
+        arr[i] = number;
+    }
+    //возвращаем массив
+    return arr;
+}
+
+//печатаем результат на экран
+Console.WriteLine($"[{String.Join(",", array)}]");
